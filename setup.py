57b1d93dda47b26d9df90fb7efff20e8f08b4363
nupursharma <nynapar03@gmail.com>
2025-06-25 15:55:34 +0530

    initial commmit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='earhquake_classification',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='nupursharma',
+    author_email='',
+    description=''
+)
